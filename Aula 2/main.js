bb7f5490a59aacfb10689fbf7e7aba7062fdd37b
gomesIgor21 <igorb.21gomes@gmail.com>
2021-09-30 15:59:13 -0300

    Exercise data types

--- Aula 2/main.js
+++ Aula 2/main.js
@@ -1,12 +1,32 @@
-var nome = "Igor"; //String
+var nome = 'Igor'; //String
 var idade = 23; //Number
 var possuiFaculdade = true; //Boolean
 var time; //Undefined
 var comida = null; //Null;
-var simbolo = Symbol() //Symbol
-var novoObjeto = {} //Object
+var simbolo = Symbol(); //Symbol
+var novoObjeto = {}; //Object
 
 console.log(typeof nome);
 console.log(typeof idade);
 console.log(typeof possuiFaculdade);
 console.log(typeof comida);
+
+//Exercicio
+
+var vString = 'Essa é uma String ';
+var vStringNumber = 'Essa é uma string com numeros: 1, 2 ,3, ...';
+var idade = 23;
+
+console.log(vString + vStringNumber);
+console.log(`Idade: ${idade}`);
+
+var nome = 'Igor ';
+var sobrenome = 'Gomes';
+
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+var vString2 = 'It\'s time';
+console.log(vString2);
+
+console.log(typeof nome);
