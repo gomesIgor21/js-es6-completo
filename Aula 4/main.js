c0ec0dfe9702adad1f86d2c5acc17029b151a2d9
gomesIgor21 <igorb.21gomes@gmail.com>
2021-09-30 17:30:31 -0300

    Comparison Operators

--- Aula 4/main.js
+++ Aula 4/main.js
@@ -6,3 +6,27 @@ if (possuiGraduacao) {
 } else {
 	console.log('Não Possui Graduação');
 }
+
+possuiGraduacao = false;
+if(!possuiGraduacao) {
+  console.log("Não possui graduação")
+}
+
+// > (Maior)/ < (menor)
+if(10 > 5) {
+  console.log("10 maior que 5");
+}
+
+// >= (Maior ou Igual)/ <= (Menor ou igual)
+if(10 >= 10) {
+  console.log("10 é igual a 10")
+}
+
+if( 10 == '10') {
+  console.log("Verdadeiro ==")
+}
+
+//Igualdade estrita valor e tipo
+if( 10 === '10') {
+  console.log("Verdadeiro ===")
+}
