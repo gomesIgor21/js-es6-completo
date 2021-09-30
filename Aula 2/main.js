10819e77aba123f5f04bd146ba1b15a0148777f3
gomesIgor21 <igorb.21gomes@gmail.com>
2021-09-30 15:42:39 -0300

    Aula 2 Data types

---
+++ Aula 2/main.js
@@ -0,0 +1,7 @@
+var nome = "Igor"; //String
+var idade = 23; //Number
+var possuiFaculdade = true; //Boolean
+var time; //Undefined
+var comida = null; //Null;
+var simbolo = Symbol() //Symbol
+var novoObjeto = {} //Object
