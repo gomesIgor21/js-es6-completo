6426057b13de1e43728bdd6c31864b96c2472e96
gomesIgor21 <igorb.21gomes@gmail.com>
2021-09-30 15:35:14 -0300

    Exercise variables

--- Aula 1/main.js
+++ Aula 1/main.js
@@ -1,5 +1,17 @@
-var nome = 'Igor';
+var nome = 'Fulano';
 var idade = 28;
 var possuiFaculdade = true;
 
-console.log(nome, idade, possuiFaculdade)
+console.log(nome, idade, possuiFaculdade);
+
+//Exercicio
+
+var meuNome = 'Igor';
+var minhaidade = 23;
+
+var comida;
+comida = 'Lasanha';
+
+var semValor1, semValor2, semValor3, semValor4, semValor5;
+
+
