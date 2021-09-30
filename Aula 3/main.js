6713b88501a91be8c155a2a3b0910513241413b0
gomesIgor21 <igorb.21gomes@gmail.com>
2021-09-30 16:10:11 -0300

    Arithmetic operators

--- Aula 3/main.js
+++ Aula 3/main.js
@@ -5,3 +5,12 @@ var exp = 2e10;
 //Precisão para até 15 dígitos
 
 console.log(`${idade}\n${goals}\n${pi}\n${exp}`);
+
+var soma = 2 + 2;
+var subtracao = 100 - 50;
+var multiplicacao = 9 * 9;
+var divisao = 100 / 2;
+var expoente = 2 ** 4;
+var modulo = 14 % 5;
+
+console.log(soma, subtracao, multiplicacao, divisao, expoente, modulo);
