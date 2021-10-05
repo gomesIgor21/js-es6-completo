86f02c9fc4ac207d2d07241ef5aa1be626e8f60d
gomesIgor21 <igorb.21gomes@gmail.com>
2021-10-05 10:18:15 -0300

    Exercises

--- Aula 5/main.js
+++ Aula 5/main.js
@@ -14,6 +14,48 @@ function imc(peso, altura) {
 var imc = imc(80, 1.7); // 80 e 1.70 são os argumentos
 console.log(imc);
 
-addEventListener('click', function(params) {
-	console.log("Clicou");	
-})
+addEventListener('click', function (params) {
+	console.log('Clicou');
+});
+
+// Exercícios
+
+function isTruthy(dado) {
+	return !!dado;
+}
+
+function perimetroQuadrado(lado) {
+	return lado * 4;
+}
+
+function nomeCompleto(nome, sobrenome) {
+	return `${nome} ${sobrenome}`;
+}
+
+function isEven(numero) {
+	var modulo = numero % 2;
+	if (modulo === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function tipoDeDado(dado) {
+	return typeof dado;
+}
+
+addEventListener('scroll', function () {
+	console.log('Igor Gomes');
+});
+
+
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+	return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+	return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
